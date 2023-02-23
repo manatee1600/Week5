92044bfb9df9609d609e00ef45e03c26233e5b36
elephant_seal <mikan.ha.orange123@gmail.com>
2023-02-23 12:52:04 +0900

    initial commit

---
+++ src/main/java/org/example/Steream.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.List;
+public class Steream {
+
+    public static void main(String[] args) {
+        List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
+        List<String> sortedResult = names.stream().sorted().toList();
+
+        long count = names.stream().filter(v -> v.startsWith("y")).count();
+
+        boolean hasYamada = names.stream().anyMatch(name -> name.equals("yamada"));
+        //List<String> result = names.stream().filter(v -> v.contains("a")).toList();
+
+        System.out.println(sortedResult);
+        System.out.println(count);
+        System.out.println(hasYamada);
+
+    }
+
+}
